f55bbcfd90de13eb110d59da61dfff1fafc26c9c
Adam Schuman <adam.schuman1512@gmail.com>
2025-01-15 17:14:46 +1100

    stopped the walnut network from being included

--- Build-Walnut-External.lua
+++ Build-Walnut-External.lua
@@ -23,7 +23,7 @@ group "Frontend/Core"
     include "Walnut/Build-Walnut.lua"
 
     -- Optional modules
-    if os.isfile("Walnut-Modules/Walnut-Networking/Build-Walnut-Networking.lua") then
-        include "Walnut-Modules/Walnut-Networking/Build-Walnut-Networking.lua"
-    end
+    -- if os.isfile("Walnut-Modules/Walnut-Networking/Build-Walnut-Networking.lua") then
+    --     include "Walnut-Modules/Walnut-Networking/Build-Walnut-Networking.lua"
+    -- end
 group ""
