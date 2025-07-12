e51a94b3e43c91e507e0aa57295007a810edf44d
0xShawn <intelbeyondrepair@gmail.com>
2025-07-12 13:57:59 -0700

    Fix semaphore reuse validation error by waiting on all fences

--- Walnut/src/Walnut/Application.cpp
+++ Walnut/src/Walnut/Application.cpp
@@ -663,6 +663,17 @@ namespace Walnut {
 			wd->ClearValue.color.float32[3] = clear_color.w;
 			if (!main_is_minimized)
 				FrameRender(wd, main_draw_data);
+			
+				
+			// Wait for all fences to avoid semaphore reuse issues in multi-viewport mode
+			for (uint32_t i = 0; i < g_MainWindowData.ImageCount; i++)
+			{
+				ImGui_ImplVulkanH_Frame* frame = &g_MainWindowData.Frames[i];
+				if (frame->Fence != VK_NULL_HANDLE)
+				{
+					vkWaitForFences(g_Device, 1, &frame->Fence, VK_TRUE, UINT64_MAX);
+				}
+			}
 
 			// Update and Render additional Platform Windows
 			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
